793425b4679c6dcb962f8c15900fb4accf447137
Teboho <leroibakiteboho@gmail.com>
2026-04-15 12:34:48 +0200

    Implement cocktail section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -10,9 +10,15 @@ const Hero = () => {
   const isMobile = useMediaQuery({ maxWidth: 767 });
 
   useGSAP(() => {
-    const heroSplit = new SplitText(".title", { type: "chars, words" });
-    const paragraphSplit = new SplitText(".subtitle", { type: "lines" });
+    const heroSplit = new SplitText(".title", {
+      type: "chars, words",
+    });
 
+    const paragraphSplit = new SplitText(".subtitle", {
+      type: "lines",
+    });
+
+    // Apply text-gradient class once before animating
     heroSplit.chars.forEach((char) => char.classList.add("text-gradient"));
 
     gsap.from(heroSplit.chars, {
@@ -41,7 +47,8 @@ const Hero = () => {
         },
       })
       .to(".right-leaf", { y: 200 }, 0)
-      .to(".left-leaf", { y: -200 }, 0);
+      .to(".left-leaf", { y: -200 }, 0)
+      .to(".arrow", { y: 100 }, 0);
 
     const startValue = isMobile ? "top 50%" : "center 60%";
     const endValue = isMobile ? "120% top" : "bottom top";
@@ -65,10 +72,9 @@ const Hero = () => {
 
   return (
     <>
-      <section id="hero"  className="noisy overflow-y-hidden">
+      <section id="hero" className="noisy">
         <h1 className="title">MOJITO</h1>
 
-        {/* leaf section */}
         <img
           src="/images/hero-left-leaf.png"
           alt="left-leaf"
@@ -80,8 +86,9 @@ const Hero = () => {
           className="right-leaf"
         />
 
-        {/* content section */}
         <div className="body">
+          {/* <img src="/images/arrow.png" alt="arrow" className="arrow" /> */}
+
           <div className="content">
             <div className="space-y-5 hidden md:block">
               <p>Cool. Crisp. Classic.</p>
@@ -104,11 +111,11 @@ const Hero = () => {
 
       <div className="video absolute inset-0">
         <video
-          src="/videos/output.mp4"
           ref={videoRef}
           muted
           playsInline
           preload="auto"
+          src="/videos/output.mp4"
         />
       </div>
     </>
